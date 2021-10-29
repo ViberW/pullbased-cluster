ba7fd2dc819a042944b59954c06ed208be48d405
viber <15779229872@163.com>
2021-10-29 13:17:48 +0800

    调整比例为1.2

--- workspace-provider/src/main/java/com/aliware/tianchi/TestServerFilter.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/TestServerFilter.java
@@ -24,8 +24,7 @@ public class TestServerFilter implements Filter, BaseFilter.Listener {
         int concurrent = ProviderManager.active.getAndIncrement();
         if (concurrent > w) {
             double r = ThreadLocalRandom.current().nextDouble(1);
-//            if (r > 1.3 - (concurrent * 1.0 / w)) {//1.3? 1.5? 1.2? -- ( 1.3* w -concurrent)/0.3*w ?
-            if (r > (1.3 * w - concurrent) / (0.3 * w)) {
+            if (r > 1.2 - (concurrent * 1.0 / w)) {//1.3? 1.5? 1.2?
                 throw new RpcException(RPCCode.FAST_FAIL,
                         "fast failure by provider to invoke method "
                                 + invocation.getMethodName() + " in provider " + invoker.getUrl());
